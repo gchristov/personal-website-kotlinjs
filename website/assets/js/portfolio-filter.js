e5e0863f791af78e526b53f50a84ba8e99bb6a9f
Georgi Christov <georgi.christov89@gmail.com>
2020-12-01 14:23:25 +0000

    Fix for localization loading and project filtering

--- website/assets/js/portfolio-filter.js
+++ website/assets/js/portfolio-filter.js
@@ -1,30 +1,27 @@
 'use strict';
 
-//Filter project cards
+// Filter project cards 
 var previousClickedMenuLink = undefined;
-$('.portfolio-menu').on('click', 'a', function(event){
+$('.portfolio-menu').on('click', 'a', function(event) {
+    // Link style
     event.preventDefault();
-
     if (previousClickedMenuLink) {
         previousClickedMenuLink.removeClass('portfolio-menu__link--active');
     }
     var link = $(event.target);
     link.addClass('portfolio-menu__link--active');
     previousClickedMenuLink = link;
-
+    // Filter
     var targetTag = $(event.target).data('portfolio-target-tag');
-    var portfolioItems = $('.portfolio-cards').children();
-
-    if (targetTag === 'all') {
-        portfolioItems.fadeIn({duration: 500});
-    } else {
-        portfolioItems.hide();
-    }
-
-    portfolioItems.each(function(index, value){
+    var portfolioItems = $('.project-card');
+    portfolioItems.each(function(index, value) {
         var item = $(value);
-        if (item.data('portfolio-tag') === targetTag) {
+        item.hide();
+    });
+    portfolioItems.each(function(index, value) {
+        var item = $(value);
+        if (item.data('portfolio-tag') === targetTag || targetTag === 'all') {
             item.fadeIn({duration: 500});
-        }
+        } 
     });
 });
