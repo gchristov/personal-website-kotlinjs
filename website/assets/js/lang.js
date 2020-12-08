e95e94a32a4b9e7c673c7e7b3dac72d0390f8053
Georgi Christov <georgi.christov89@gmail.com>
2020-12-08 10:32:38 +0000

    Replaced flags API

--- website/assets/js/lang.js
+++ website/assets/js/lang.js
@@ -2,5 +2,5 @@
 
 function loadLanguage(languageCode, languageTitle) {
 	$("[data-localize]").localize("assets/lang/lang", { language: languageCode });
-	$("#languageSwitcher").html("<img id='languageSwitcherFlag' src='https://www.countryflags.io/" + languageCode + "/shiny/24.png'/>" + languageTitle);
+	$("#languageSwitcher").html("<img id='languageSwitcherFlag' src='https://restcountries.eu/data/" + languageCode + ".svg'/>" + languageTitle);
 }
