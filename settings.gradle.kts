de667b6d62e05c4c34e4d31acadca525f694ee93
Georgi Christov <ga.christov@gmail.com>
2024-10-23 00:17:06 +0200

    Add root gradle scan publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,19 @@
+plugins {
+    // Allows publishing build scans when common tasks run from the project root, eg jsTest
+    id("com.gradle.develocity") version("3.18.1")
+}
+
 rootProject.name = "personal-website-kotlinjs"
 
 // Add or remove projects here from the common build. Alternatively, each project can be opened in isolation.
 includeBuild("gradle-plugins")
 includeBuild("landing-page-web")
 includeBuild("proxy-web")
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+        publishing.onlyIf { true }
+    }
+}
