bfbe90ceb972eed226adfe1616d0952c306ace17
Georgi Christov <ga.christov@gmail.com>
2025-11-19 22:26:13 +0100

    Create gradle conventions plugin

---
+++ gradle-plugins/conventions/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `kotlin-dsl`
+}
+
+gradlePlugin {
+    plugins.register("base-browser-plugin") {
+        id = "base-browser-plugin"
+        implementationClass = "com.gchristov.personal.gradleplugins.BaseBrowserPlugin"
+    }
+    plugins.register("browser-binary-plugin") {
+        id = "browser-binary-plugin"
+        implementationClass = "com.gchristov.personal.gradleplugins.BrowserBinaryPlugin"
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+}
--- gradle-plugins/settings.gradle.kts
+++ gradle-plugins/settings.gradle.kts
@@ -1,3 +1,11 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+dependencyResolutionManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "gradle-plugins"
+
+include(":conventions")
