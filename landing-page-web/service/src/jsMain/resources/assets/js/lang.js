f7aa76ac11f5261cd109020bf9e1206c9520f7db
Georgi Christov <ga.christov@gmail.com>
2026-06-12 09:48:47 +0200

    update copyright date

--- landing-page-web/service/src/jsMain/resources/assets/js/lang.js
+++ landing-page-web/service/src/jsMain/resources/assets/js/lang.js
@@ -1,6 +1,13 @@
 'use strict';
 
 function loadLanguage(languageCode, languageTitle) {
-	$("[data-localize]").localize("assets/lang/lang", { language: languageCode });
+	$("[data-localize]").localize("assets/lang/lang", { language: languageCode }).localizePromise.done(function() {
+		var copyrightEl = $("[data-localize='contact_copyright']");
+		if (copyrightEl.length) {
+			var currentYear = new Date().getFullYear();
+			var text = copyrightEl.text();
+			copyrightEl.text(text.replace("%year%", currentYear));
+		}
+	});
 	$("#languageSwitcher").html("<img id='languageSwitcherFlag' src='assets/img/flag-" + languageCode + ".svg'/>" + languageTitle);
 }
